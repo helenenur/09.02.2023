778261b41bdd10ce3c2ecf87b43558a06a5b322b
helenenur <helenenur95@gmail.com>
2023-02-09 15:32:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+function mySum(a ){
+  let result = a * b ;
+  return result;
+ }
+
+ let a = (5 , 5);
+ let b =  (6, 6);
+
+ if (a <=b){
+  console.log("Yes it does");
+ }
+ else {
+  console.log("No it does'n");
+ }
+
+ function getNumber(){
+  console.log(5 + 5);
+ }
+
+ function nameOfArea (a , b) {
+  console.log( a * b );
+ }
+
+ nameOfArea (5 , 5);
+ nameOfArea (10, 10);
+ 
+ 
+
+ function countup(a,b) {
+  console.log(5 , 10);
+ }
+ 
+ function countup(a, b) { 
+  let ot = a; 
+  while (ot <= b) { 
+    console.log(ot) 
+    ot++; 
+  } 
+} 
+countup(5,10) 
+countup(-10,10)
+
+function countup (c, d) {
+  let ot = c;
+  while(ot >=d) {
+    ot --;
+  }
+}
+
+
+
+function countdown (e, i) {
+  let ot = e;
+  while(ot <= e){
+    ot ++;
+  }
+}
+
+function countdown (b, c){
+  let ot = b;
+  while(ot >=c){
+    ot --;
+  }
+}
+
+function countup (a, b){
+  for (let i = a; i <= b; i++)
+  console.log(i)
+}
+
+count(5, 10);
+count (10, 5);
+
+function count(a, b) {
+  if (a < b) {
+    countup(a ,b) ;
+  }
+else{
+  countdown (a, b);
+}
+}
+
+let names = ["Aidai",  "Bakyt" , "Joldosh"];
+
+
+
+
+
